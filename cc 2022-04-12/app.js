8a10fbb7ce6b548d86f089ba3cf8fdcde0c093ed
Mdue17 <dubemdue99@gmail.com>
2022-04-13 16:44:16 +0200

    added if else

--- cc 2022-04-12/app.js
+++ cc 2022-04-12/app.js
@@ -10,7 +10,9 @@
 //p:
 // make a function that takes in an array
     function doubled(arr){
+        if(arr=== undefined ||  arr.length=== 0){
+            console.log('array is invalid')
+        }else{
         return arr.map(element=>element*2)
     }
-//map thriugh the array and multiply each element by 2
-doubled([2,4,6])
+}
