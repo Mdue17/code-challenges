a4293504472124ed2850703ee31542631c4d9b63
Mdue17 <dubemdue99@gmail.com>
2022-04-25 22:50:40 +0200

    bob the builder math

---
+++ cc 2022-05-16/app.js
@@ -0,0 +1,11 @@
+//Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the //cuboid. Write a function to help Bob with this calculatiion
+
+function bobBuilder (l,w,h){
+    //p: intergers as length width and height for cuboidal
+    //r: the volume of the cuboid
+    //e: given (300,210,90) = vol
+    //p: formula for volume of cube l * w * h
+    let result = l*w*h
+    return result
+
+}
