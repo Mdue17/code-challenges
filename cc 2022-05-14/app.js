1eaabaa97ff4d83a1e18c31922f39aae7c6c9ce3
Mdue17 <dubemdue99@gmail.com>
2022-05-14 22:46:16 +0200

    boolean to string

---
+++ cc 2022-05-14/app.js
@@ -0,0 +1,8 @@
+// Implement a function which convert the given boolean value into its string representation.
+ function booleanToStr (b){
+    //p://Only valid inputs will be given.
+    //r: get 'true || "false"
+    //e: 2>5 => false c.log "false"
+    //p:take the given input and use toString method to get string of boolean..
+          return b.toString()
+ }
