8679c3c9cf95ad56a6b07d8a98c849d698239595
Mdue17 <dubemdue99@gmail.com>
2022-05-19 22:27:13 +0200

    return input as negative

---
+++ cc 2022-05-19/app.js
@@ -0,0 +1,12 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+    function makeNegative(num){
+        //prep
+        //p:a number thats positive, can be whole, can have decimals
+        //r: a negative number of the input
+        //e:makeNegative(1);    // return -1
+        //p:use Math.abs(-)
+
+        return Math.abs(-num) * -1
+
+    }
