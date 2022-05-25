5c43881c273d306cdd4e2bd1da9f24f545d9e20b
Mdue17 <dubemdue99@gmail.com>
2022-05-25 23:38:32 +0200

    function basics

---
+++ cc 2022-05-25/app.js
@@ -0,0 +1,6 @@
+//In JavaScript, your code is running in a function, let us step by step complete your first JS function.
+
+function helloWorld(){
+    var string = "hello world!!"
+    console.log(string)
+}
