a48ab82866a2b39c8f1bc5bb5c9f9077096663a8
Mdue17 <dubemdue99@gmail.com>
2022-05-31 22:45:20 +0200

    num to str p2

---
+++ cc 2022-05-31/app.js
@@ -0,0 +1,14 @@
+// We need a function that can transform a number into a string.
+
+function numberToString(num){
+    //PREP
+    //P: natural numbers 
+    //R:same numbers but as strings
+    //E: 123 -> "123"
+    //P: use toString method
+
+    return num.toString()
+}
+
+// Examples:
+// 123 --> "123"
